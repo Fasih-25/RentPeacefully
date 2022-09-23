80de6cd7404ed16b260ceeec6a89cb2dd372a68e
Fasih <fasih.rana@regex.global>
2022-09-23 17:03:02 +0500

    .................

--- src/SignUp.js
+++ src/SignUp.js
@@ -4,29 +4,32 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import { Select, Option } from "@material-tailwind/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEnvelope,faLock, faUserAlt} from '@fortawesome/free-solid-svg-icons';
+import {faEnvelope,faLock, faPhoneAlt, faUserAlt} from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-    const [usertype, setusertype] = useState("")
-    const [username, setusername] = useState("")
-    const [lastName, setlastName] = useState("")
+    const [userType, setUserType] = useState("")
+    const [username, setUserName] = useState("")
+    const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
+    const [phoneNo, setPhoneNo] = useState("")
     const [password, setPassword] = useState("")
-    let item={username, email, password, usertype}
+    const [message, setMessage] = useState("");
     let navigate = useNavigate();
    async function handleSelect()
     {
         // console.warn(username, lastName, email,password)
-        console.info(item)
+        let item={username, email, password, phoneNo}
+        console.log(item);
         
-        let result = await fetch("localhost:3001/user/register",{
-            method:'POST',
+        let result = await fetch("https://nodejs-rental-api.herokuapp.com/user/register",{
+            method:"POST",
+            body:JSON.stringify(item),
             headers:{
-                "Content-Type":"application/json",
-                "Accept":"application/json"
-            },
-            body:JSON.stringify(item)
+                "Content-Type":'application/json',
+                "Accept":'application/json'
+            }
+        
         })
         // .then((Response) => Response.json())
         // .then((result) => {
@@ -43,6 +46,40 @@ export default function SignUp() {
         console.warn("Result = ",result)
         
     }
+
+// let handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       let res = await fetch("localhost:3001/user/register", {
+//         mode: 'no-cors',
+//         method: "POST",
+//         body: JSON.stringify({
+//           username: username,
+//           email: email,
+//           password: password,
+//         //   phoneNo: phoneNo,
+//         //   userType: "",
+//         }),
+//         // headers:{
+//         //     "Content-Type":'application/json',
+//         //     "Accept":'application/json'
+//         // }
+                    
+//       });
+//       let resJson = await res.json();
+//       if (res.status === 200) {
+//         setUserName("");
+//         setEmail("");
+//         setPhoneNo("");
+//         setUserType("");
+//         setMessage("User created successfully");
+//       } else {
+//         setMessage("Some error occured");
+//       }
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
     
     // const [retypePassword, setretypePassword] = useState("")
 
@@ -58,42 +95,42 @@ export default function SignUp() {
             </div>  
             <div className="w-full px-6 py-4 outline-sky-600 border overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg ">
                 <form >
-                  <div className="w-full">
-                    <Select className='flex justify-center text-center items-center' onChange={(e)=>setusertype(e.target.value)}>
-                      <Option value={"i am Tenant"} Select="Selected" className=' text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Tenant</Option>
-                      <Option value={"i am Landlord"} className='text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Landlord</Option>
-                      <Option value={"i am Service Provider"} className='text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Service Provider</Option>
-                      <Option value={"i am Real Estate Agent"} className='text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Real Estate Agent</Option>
+                  {/* <div className="w-full">
+                    <Select className='flex justify-center text-center items-center' onChange={(e)=>setUserType(e.target.value)}>
+                      <Option value={userType} select="Selected" className=' text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Tenant</Option>
+                      <Option value={userType} className='text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Landlord</Option>
+                      <Option value={userType} className='text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Service Provider</Option>
+                      <Option value={userType} className='text-blue-500 font-bold hover:bg-teal-600 hover:text-white'>I am a Real Estate Agent</Option>
                     </Select>
-                  </div>
+                  </div> */}
                     <div>
                         <div className="flex flex-row items-center justify-center mt-4">
                             <FontAwesomeIcon icon={faUserAlt} className=" text-3xl text-gray-400 "></FontAwesomeIcon>
                             <input
                                 type="text"
-                                placeholder='First Name'
+                                placeholder='Name'
                                 required
-                                // name="username"
+                                name="username"
                                 value={username}
-                                onChange={(e)=>setusername(e.target.value)}
+                                onChange={(e)=>setUserName(e.target.value)}
                                 className="block w-full px-2 xl:!py-3 py-2 mx-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             />
                         </div>
                     </div>
-                    <div>
+                    {/* <div>
                         <div className="flex flex-row items-center justify-center mt-4">
                             <FontAwesomeIcon icon={faUserAlt} className=" text-3xl text-gray-400 "></FontAwesomeIcon>
                             <input
                                 type="text"
                                 required
                                 placeholder='Last Name'
-                                // name="lastName"
+                                name="lastName"
                                 value={lastName}
-                                onChange={(e)=>setlastName(e.target.value)}
+                                onChange={(e)=>setLastName(e.target.value)}
                                 className="block w-full px-2 xl:!py-3 py-2 mx-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             />
                         </div>
-                    </div>
+                    </div> */}
                     <div className="mt-4">
                         <div className="flex flex-row items-center justify-center mt-4">
                             <FontAwesomeIcon icon={faEnvelope} className=" text-3xl text-gray-400 "></FontAwesomeIcon>
@@ -101,9 +138,23 @@ export default function SignUp() {
                                 type="email"
                                 required
                                 placeholder='Email'
-                                // name="email"
+                                name="email"
                                 value={email}
                                 onChange={(e)=>setEmail(e.target.value)}
+                                className="block w-full px-2 xl:!py-3 py-2 mx-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            />
+                        </div>
+                    </div>
+                    <div className="mt-4">
+                        <div className="flex flex-row items-center justify-center mt-4">
+                            <FontAwesomeIcon icon={faPhoneAlt} className=" text-3xl text-gray-400 "></FontAwesomeIcon>
+                            <input
+                                type="Text"
+                                required
+                                placeholder='Phone Number'
+                                name="phoneNo"
+                                value={phoneNo}
+                                onChange={(e)=>setPhoneNo(e.target.value)}
                                 className="block w-full px-2 xl:!py-3 py-2 mx-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             />
                         </div>
@@ -115,7 +166,7 @@ export default function SignUp() {
                                 type="password"
                                 required
                                 placeholder='Password'
-                                // name="password"
+                                name="password"
                                 value={password}
                                 onChange={(e)=>setPassword(e.target.value)}
                                 className="block w-full px-2 xl:!py-3 py-2 mx-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -123,7 +174,7 @@ export default function SignUp() {
                         </div>
                     </div>
                     <div className="mt-4">
-                        <div className="flex flex-row items-center justify-center mt-4">
+                        {/* <div className="flex flex-row items-center justify-center mt-4">
                             <FontAwesomeIcon icon={faLock} className=" text-3xl text-gray-400 "></FontAwesomeIcon>
                             <input
                                 type="password"
@@ -132,15 +183,16 @@ export default function SignUp() {
                                 // name="password_confirmation" 
                                 className="block w-full xl:!py-3 px-2 py-2 mx-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             />
-                        </div>
+                        </div> */}
                         {/* {(setPassword !== setretypePassword ) ? (<span className='text-red-600 font-semibold'>Password dosen't matched</span>)  : ("")} */}
                     </div>
                   
                     <div className="flex items-center mt-4">
-                        <button onClick={handleSelect} className="w-full px-4 py-3 mx-5  tracking-wide text-white transition-colors duration-200 transform bg-teal-500 shadowBox  rounded-md focus:outline-none">
+                        <button onClick={handleSubmit} className="w-full px-4 py-3 mx-5  tracking-wide text-white transition-colors duration-200 transform bg-teal-500 shadowBox  rounded-md focus:outline-none">
                             CREATE NEW ACCOUNT
                         </button>
                     </div>
+                    <div className="message">{message ? <p>{message}</p> : null}</div>
                     <hr className='mt-3' />
                 </form>
                 <div className="mt-4 text-grey-600 text-center">
